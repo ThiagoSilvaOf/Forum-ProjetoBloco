fceffc614b66743dcbe7779644238ad415f9a0e5
Thiago Silva <titusilvs91@gmail.com>
2024-04-28 00:47:24 -0300

    Consumindo dados firebase

--- forumgamificada/src/pages/Home.jsx
+++ forumgamificada/src/pages/Home.jsx
@@ -6,6 +6,22 @@ import Button from "../components/Button";
 const Home = () => {
   const { desconectar } = useAuth();
   const navigate = useNavigate();
+
+  const [publicacao, setPublicacao] = React.useState(null);
+
+  const baseUrl =
+    "https://dialogos-diversos-default-rtdb.asia-southeast1.firebasedatabase.app";
+
+  React.useEffect(() => {
+    fetch(`${baseUrl}/publicacoes.json`)
+      .then(async (resp) => {
+      const respPublicao = await resp.json();
+      const publicacoesConvertidas = Object.values(respPublicao);
+      console.log(publicacoesConvertidas);
+      setPublicacao(publicacoesConvertidas);
+    });
+  }, []);
+  
   return (
     <div>
       <h2>HOME</h2>
